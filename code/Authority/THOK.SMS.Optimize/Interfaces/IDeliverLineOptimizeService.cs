7fd195c8a5bf338970019ef93c43ef7a83232afd
zqb <940658539@qq.com>
2014-05-22 10:21:27 +0800

    修改 SMS 配送线路分配（未完成）

--- code/Authority/THOK.SMS.Optimize/Interfaces/IDeliverLineOptimizeService.cs
+++ code/Authority/THOK.SMS.Optimize/Interfaces/IDeliverLineOptimizeService.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using THOK.SMS.DbModel;
+using THOK.Wms.DbModel;
 
 namespace THOK.SMS.Optimize.Interfaces
 {
-     public interface IDeliverLineOptimizeService
+    public interface IDeliverLineOptimizeService : IService<SortOrder>
     {
+         object GetOrderInfo();
+
+         object GetDeliverLine(string orderDate);
+
          bool OptimizeAllot(int batchNo, out string strResult);
     }
 }
---
+++ code/Authority/Wms/Controllers/SMS/SortAllotController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Practices.Unity;
+using THOK.SMS.Optimize.Interfaces;
+
+namespace Wms.Controllers.SMS
+{
+    public class SortAllotController : Controller
+    {
+        [Dependency]
+        public IDeliverLineOptimizeService DeliverLineOptimizeService { get; set; }
+        //
+        // GET: /SortAllot/
+
+        public ActionResult Index(string moduleID)
+        {
+            ViewBag.hasSearch = true;
+            ViewBag.hasAdd = true;
+            ViewBag.hasEdit = true;
+            ViewBag.hasDelete = true;
+            ViewBag.hasPrint = true;
+            ViewBag.hasHelp = true;
+            ViewBag.ModuleID = moduleID;
+            return View();
+        }
+
+        public ActionResult OrderInformation()
+        {
+            var orderInfo = DeliverLineOptimizeService.GetOrderInfo();
+            return Json(orderInfo, "text", JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetDeliverLine(int page, int rows, string orderDate)
+        {
+            if (orderDate == null)
+            {
+                return null;
+            }
+            var deliverLineDetail = DeliverLineOptimizeService.GetDeliverLine(orderDate);
+            return Json(deliverLineDetail, "text", JsonRequestBehavior.AllowGet);
+        }
+    }
+}

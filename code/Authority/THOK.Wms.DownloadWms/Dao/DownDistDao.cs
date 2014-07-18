84c063b9759e9674016b6f3040ed01c5b736d259
zengwh <545510993@qq.com>
2014-07-18 10:03:57 +0800

    调整分拣订单下载（判断是否仓储一体化）

--- code/Authority/THOK.Wms.DownloadWms/Dao/DownDistDao.cs
+++ code/Authority/THOK.Wms.DownloadWms/Dao/DownDistDao.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using THOK.Util;
 using System.Data;
+using THOK.Wms.DownloadWms.Dao;
 
 namespace THOK.WMS.DownloadWms.Dao
 {
@@ -50,6 +51,77 @@ namespace THOK.WMS.DownloadWms.Dao
        {
            string sql = "SELECT COM_CODE FROM BI_COMPANY";
            return this.ExecuteScalar(sql).ToString();
+       }
+
+       #endregion
+
+
+
+
+       #region   纯分拣数据下载
+
+       public string dbTypeName = "";
+       public string SalesSystemDao()
+       {
+           SysParameterDao parameterDao = new SysParameterDao();
+           Dictionary<string, string> parameter = parameterDao.FindParameters();
+
+           //仓储业务数据接口服务器数据库类型
+           if (parameter["SalesSystemDBType"] != "")
+               dbTypeName = parameter["SalesSystemDBType"];
+
+           return dbTypeName;
+       }
+
+
+       /// <summary>
+       /// 根据条件下载区域表
+       /// </summary>
+       /// <returns></returns>
+       public DataTable FindArea()
+       {
+           string sql = "";
+           dbTypeName = this.SalesSystemDao();
+           switch (dbTypeName)
+           {
+
+               case "gzqdn-oracle":
+                   sql = @"SELECT DISTINCT 1 AS AREACODE,'全部线路' AS AREANAME,0 AS SORTID
+                            FROM V_WMS_DELIVER_LINE WHERE ISACTIVE = '1'";
+                   break;
+
+               case "yzyc-db2": //永州烟草
+                   sql = string.Format(@"SELECT AREACODE, AREANAME,0 AS SORTID " +
+                           " FROM OUKANG.OUKANG_REGION");
+                   break;
+
+
+               default://默认
+
+                   break;
+           }
+           return this.ExecuteQuery(sql).Tables[0];
+       }
+       #endregion
+
+       #region   插入数据
+       //区域
+       public void SynchronizeArea(DataTable areaTable)
+       {
+           DateTime dt = new DateTime();
+           foreach (DataRow row in areaTable.Rows)
+           {
+               string sql = "IF '{0}' IN (SELECT dist_code FROM wms_deliver_dist) " +
+                               "BEGIN " +
+                                   "UPDATE wms_deliver_dist SET dist_name = '{1}' WHERE dist_code = '{0}' " +
+                               "END " +
+                            "ELSE " +
+                               "BEGIN " +
+                                   "INSERT wms_deliver_dist VALUES ('{0}','{1}','{2},'{3}','{4},'{5}','{6},'{7}','{8},'{9}') " +
+                               "END";
+               sql = string.Format(sql, row["AREACODE"], ' ', row["AREANAME"], ' ', ' ', ' ', ' ', '1', dt, row["SORTID"]);
+               ExecuteNonQuery(sql);
+           }
        }
 
        #endregion
---
+++ code/Authority/THOK.Wms.DownloadWms/Dao/DownSalesSystemDao.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using THOK.Util;
+using System.Data;
+using DBRabbit;
+using THOK.WMS.DownloadWms.Dao;
+
+namespace THOK.Wms.DownloadWms.Dao
+{
+    public class DownSalesSystemDao : BaseDao
+    {
+        /// <summary>
+        /// 删除7天之前的数据
+        /// </summary>
+        public void DeleteHistory(string orderDate)
+        {
+            try
+            {
+                using (PersistentManager dbPm = new PersistentManager("master_1"))
+                {
+                    DownRouteDao dao = new DownRouteDao();
+                    dao.SetPersistentManager(dbPm);
+                    dao.DeleteHistory(orderDate);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("删除数据失败！原因：" + e.Message);
+            }
+        }
+    }
+}

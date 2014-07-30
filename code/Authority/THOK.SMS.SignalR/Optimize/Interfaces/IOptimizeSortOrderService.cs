8f0ceab5eb16963ed96eb176f825957c52a1b991
Endis <zhengzhibin@skyseaok.com>
2014-07-30 12:00:28 +0800

    THOK.SMS.SignalR类库添加 Signed-off-by: Endis <zhengzhibin@skyseaok.com>

---
+++ code/Authority/THOK.SMS.SignalR/Optimize/Interfaces/IOptimizeSortOrderService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using THOK.SMS.SignalR.Model;
+using System.Threading;
+
+namespace THOK.SMS.SignalR.Optimize.Interfaces
+{
+    public interface IOptimizeSortOrderService
+    {
+        void Optimize(string connectionId, ProgressState ps, CancellationToken cancellationToken, string sortBatchId);
+    }
+}
---
+++ code/Authority/THOK.SMS.SignalR/Optimize/Service/OptimizeSortOrderService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using THOK.SMS.SignalR.Connection;
+using THOK.SMS.SignalR.Optimize.Interfaces;
+using THOK.SMS.SignalR.Model;
+using System.Threading;
+
+namespace THOK.SMS.SignalR.Optimize.Service
+{
+    public class OptimizeSortOrderService : Notifier<OptimizeSortOrderConnection>, IOptimizeSortOrderService
+    {
+        public void Optimize(string connectionId, ProgressState ps, CancellationToken cancellationToken, string sortBatchId)
+        {
+            ps.State = StateType.Start;
+            ps.Messages.Add("开始优化");
+            NotifyConnection(ps.Clone());
+        }
+
+        void StateTypeForProcessing(ProgressState ps, string TotalName, int TotalValue, string CurrentName, int CurrentValue)
+        {
+            ps.State = StateType.Processing;
+            ps.TotalProgressName = TotalName;
+            ps.TotalProgressValue = TotalValue;
+            ps.CurrentProgressName = CurrentName;
+            ps.CurrentProgressValue = CurrentValue;
+            NotifyConnection(ps.Clone());
+        }
+    }
+}

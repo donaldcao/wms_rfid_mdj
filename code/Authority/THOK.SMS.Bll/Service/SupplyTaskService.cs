f8d075c8f93452bbd49863a09b50c5534bc57033
Dong-Yang <772369620@qq.com>
2014-08-22 17:48:41 +0800

    自动补货计划

--- code/Authority/THOK.SMS.Bll/Service/SupplyTaskService.cs
+++ code/Authority/THOK.SMS.Bll/Service/SupplyTaskService.cs
@@ -7,6 +7,7 @@ using Microsoft.Practices.Unity;
 using THOK.Common.Entity;
 using System.Data;
 using THOK.SMS.Dal.EntityRepository;
+using THOK.Wms.Dal.Interfaces;
 
 namespace THOK.SMS.Bll.Service
 {
@@ -16,6 +17,8 @@ namespace THOK.SMS.Bll.Service
         public ISupplyTaskRepository SupplyTaskRepository { get; set; }
         [Dependency]
         public IChannelRepository ChannelRepository { get; set; }
+        [Dependency]
+        public ISortingLineRepository SortingLineRepository { get; set; }
 
         protected override Type LogPrefix
         {
@@ -27,6 +30,7 @@ namespace THOK.SMS.Bll.Service
         {
             var supplyTaskQuery = SupplyTaskRepository.GetQueryable();
             var channelQuery = ChannelRepository.GetQueryable();
+            var sortingLineQuery = SortingLineRepository.GetQueryable();
 
             var supplyTaskDetail1 = supplyTaskQuery;
             if (supplyTask.SupplyId != null && supplyTask.SupplyId != 0)
@@ -48,17 +52,34 @@ namespace THOK.SMS.Bll.Service
             {
                 supplyTaskDetail4 = supplyTaskDetail3.Where(s => s.ChannelCode.Contains(supplyTask.ChannelCode)).OrderBy(s => s.Id);
             }
-            int total = supplyTaskDetail4.Count();
-            var supplyTasksArray = supplyTaskDetail4.OrderBy(s => s.Id).Skip((page - 1) * rows).Take(rows)
+            var supplyTaskDetail5 = supplyTaskDetail4;
+            if (supplyTask.SortingLineCode != "")
+            {
+                supplyTaskDetail5 = supplyTaskDetail4.Where(s => s.SortingLineCode.Contains(supplyTask.SortingLineCode)).OrderBy(s => s.Id);
+            }
+            var supplyTaskDetail6 = supplyTaskDetail5;
+            if (supplyTask.GroupNo != null&&supplyTask.GroupNo!=0)
+            {
+                supplyTaskDetail6 = supplyTaskDetail5.Where(s => s.GroupNo==supplyTask.GroupNo).OrderBy(s => s.Id);
+            }
+            int total = supplyTaskDetail6.Count();
+            var supplyTasksArray = supplyTaskDetail6.OrderBy(s => s.Id).Skip((page - 1) * rows).Take(rows)
                 .Select(s => new
                 {
                     s.Id,
                     s.SupplyId,
                     s.PackNo,
+                    s.SortingLineCode,
+                    SortingLineName = sortingLineQuery.Where(sl => sl.SortingLineCode == sl.SortingLineCode).Select(sl => sl.SortingLineName),
+                    GroupNo = s.GroupNo == 1 ? "A线" : "B线",
                     s.ProductCode,
                     s.ProductName,
                     s.ChannelCode,
-                    s.ChannelName
+                    s.ChannelName,
+                    s.ProductBarcode,
+                    s.OriginPositionAddress,
+                    s.TargetSupplyAddress,
+                    Status = s.Status == "1" ? "已下单" : "未下单"
                 }).ToArray();
             return new { total, rows = supplyTasksArray };
         }
@@ -67,6 +88,7 @@ namespace THOK.SMS.Bll.Service
         {
             IQueryable<SupplyTask> SupplyTasksQuery = SupplyTaskRepository.GetQueryable();
             IQueryable<Channel> channel = ChannelRepository.GetQueryable();
+            //IQueryable<> sortingLineQuery = SortingLineRepository.GetQueryable();
             var supplyTasksDetail = SupplyTaskRepository.GetQueryable();
             var supplyTasks = supplyTasksDetail.Where(s => s.SupplyId == supplyTask.SupplyId);
             int total = supplyTasks.Count();
@@ -76,18 +98,31 @@ namespace THOK.SMS.Bll.Service
                     s.Id,
                     s.SupplyId,
                     s.PackNo,
+                    s.SortingLineCode,
+                    //SortingLineName = sortingLineQuery.Where(sl => sl.SortingLineCode == sl.SortingLineCode).Select(sl => sl.SortingLineName),
+                    GroupNo = s.GroupNo == 1 ? "A线" : "B线",
                     s.ProductCode,
                     s.ProductName,
                     s.ChannelCode,
-                    s.ChannelName
+                    s.ChannelName,
+                    s.ProductBarcode,
+                    s.OriginPositionAddress,
+                    s.TargetSupplyAddress,
+                    Status = s.Status == "1" ? "已下单" : "未下单"
                 });
             System.Data.DataTable dt = new System.Data.DataTable();
-            dt.Columns.Add("分拣批次ID", typeof(int));
+            dt.Columns.Add("补货编码", typeof(int));
             dt.Columns.Add("烟包包号", typeof(int));
+            dt.Columns.Add("分拣线代码", typeof(string));
+            dt.Columns.Add("分拣线组号", typeof(int));
             dt.Columns.Add("烟道代码", typeof(string));
             dt.Columns.Add("烟道名称", typeof(string));
             dt.Columns.Add("商品代码", typeof(string));
             dt.Columns.Add("商品名称", typeof(string));
+            dt.Columns.Add("商品条码", typeof(string));
+            dt.Columns.Add("起始位置", typeof(int));
+            dt.Columns.Add("目标地址", typeof(int));
+            dt.Columns.Add("状态", typeof(string));
 
             foreach (var item in supplyTasksArray)
             {

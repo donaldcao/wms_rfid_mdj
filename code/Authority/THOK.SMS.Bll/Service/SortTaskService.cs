0e408a639506c339a9582e59575952209821a290
zqloon <zqloon@gmail.com>
2014-08-25 11:01:06 +0800

    补货计划生成修改

--- code/Authority/THOK.SMS.Bll/Service/SortTaskService.cs
+++ code/Authority/THOK.SMS.Bll/Service/SortTaskService.cs
@@ -32,75 +32,85 @@ namespace THOK.SMS.Bll.Service
         #endregion
         public bool CreateSortSupply(int supplyCachePositionNo, int vacancyQuantity, DateTime orderdate, int batchNO, out string errorInfo)
         {
-            var SortSupplyQuery = SortSupplyRepository.GetQueryable();
-            var SupplyTaskQuery = SupplyTaskRepository.GetQueryable();
-            var ProductQuery=ProductRepository.GetQueryable();
-            var SortTasks = SortSupplyQuery.Where
-                (s => s.SortBatch.OrderDate == orderdate && s.SortBatch.BatchNo == batchNO && s.Channel.SupplyCachePosition == supplyCachePositionNo)
-                .Join(ProductQuery,
-                  a=>a.ProductCode,
-                  b=>b.ProductCode,
-                  (a,b)=>new
-                  {
-                     a.Id,
-                     a.PackNo,
-                     a.SortBatch.SortingLineCode,
-                     a.Channel.GroupNo,
-                     a.Channel.ChannelCode,
-                     a.Channel.ChannelName,
-                     a.ProductCode,
-                     a.ProductName,
-                     ProductBarcode = (b.PieceBarcode == null || b.PieceBarcode == "") ? b.OneProjectBarcode.Substring(7, 6).ToString() : b.PieceBarcode.Substring(0, 6).ToString(),
-                     a.Channel.SupplyAddress
-                  })
-                 .Select(s => new
-                 {
-                     s.Id,
-                     s.PackNo,
-                     s.SortingLineCode,
-                     s.GroupNo,
-                     s.ChannelCode,
-                     s.ChannelName,
-                     s.ProductCode,
-                     s.ProductName,
-                     s.ProductBarcode,
-                     OriginPositionAddress = 0,
-                     s.SupplyAddress,
-                     status = "0"
-                 }).OrderBy(s=>s.Id);
-            //产生任务量=空位数（vacancyQuantity）-未下单总量（status=0）任务SupplyId=sum(补货计划所有任务)+1
-            var  SortTask = SortTasks.Where(a => !(SupplyTaskQuery.Select(s => s.SupplyId)).Contains(a.Id)).ToArray();
-            int notOrderQuantity = SupplyTaskQuery
-                .Join(ChannelRepository.GetQueryable()
-                , a => a.ChannelCode
-                , b => b.ChannelCode
-                , (a, b) => new { b.SupplyCachePosition ,a.Status}).Where(a => a.SupplyCachePosition == supplyCachePositionNo&&a.Status=="0").Count();
-            if (SortTask.Count() > 0)
+            try
             {
-                SupplyTask supplyTask = new SupplyTask();
-                for (int i = 0; i < vacancyQuantity - notOrderQuantity; i++)
+                var SortSupplyQuery = SortSupplyRepository.GetQueryable();
+                var SupplyTaskQuery = SupplyTaskRepository.GetQueryable();
+                var ProductQuery = ProductRepository.GetQueryable();
+                var SortTasks = SortSupplyQuery.Where
+                    (s => s.SortBatch.OrderDate == orderdate && s.SortBatch.BatchNo == batchNO && s.Channel.SupplyCachePosition == supplyCachePositionNo)
+                    .Join(ProductQuery,
+                      a => a.ProductCode,
+                      b => b.ProductCode,
+                      (a, b) => new
+                      {
+                          a.Id,
+                          a.PackNo,
+                          a.SortBatch.SortingLineCode,
+                          a.Channel.GroupNo,
+                          a.Channel.ChannelCode,
+                          a.Channel.ChannelName,
+                          b.ProductCode,
+                          b.ProductName,
+                          ProductBarcode = (b.PieceBarcode == null || b.PieceBarcode == "") ? b.OneProjectBarcode.Substring(7, 6).ToString() : b.PieceBarcode.Substring(0, 6).ToString(),
+                          a.Channel.SupplyAddress
+                      })
+                     .Select(s => new
+                     {
+                         s.Id,
+                         s.PackNo,
+                         s.SortingLineCode,
+                         s.GroupNo,
+                         s.ChannelCode,
+                         s.ChannelName,
+                         s.ProductCode,
+                         s.ProductName,
+                         s.ProductBarcode,
+                         OriginPositionAddress = 0,
+                         s.SupplyAddress,
+                         status = "0"
+                     }).OrderBy(s => s.Id);
+                //产生任务量=空位数（vacancyQuantity）-未下单总量（status=0）任务SupplyId=sum(补货计划所有任务)+1
+                var SortTask = SortTasks.Where(a => !(SupplyTaskQuery.Select(s => s.SupplyId)).Contains(a.Id)).ToArray();
+                int notOrderQuantity = SupplyTaskQuery
+                    .Join(ChannelRepository.GetQueryable()
+                    , a => a.ChannelCode
+                    , b => b.ChannelCode
+                    , (a, b) => new { b.SupplyCachePosition, a.Status }).Where(a => a.SupplyCachePosition == supplyCachePositionNo && a.Status == "0").Count();
+                if (SortTask.Count() > 0 && vacancyQuantity - notOrderQuantity > 0)
                 {
-                    supplyTask.SupplyId = SortTask[i].Id;
-                    supplyTask.PackNo = SortTask[i].PackNo;
-                    supplyTask.SortingLineCode = SortTask[i].SortingLineCode;
-                    supplyTask.GroupNo = SortTask[i].GroupNo;
-                    supplyTask.ChannelCode = SortTask[i].ChannelCode;
-                    supplyTask.ChannelName = SortTask[i].ChannelName;
-                    supplyTask.ProductCode = SortTask[i].ProductCode;
-                    supplyTask.ProductName = SortTask[i].ProductName;
-                    supplyTask.ProductBarcode = SortTask[i].ProductBarcode;
-                    supplyTask.OriginPositionAddress = SortTask[i].OriginPositionAddress;
-                    supplyTask.TargetSupplyAddress = SortTask[i].SupplyAddress;
-                    supplyTask.Status = SortTask[i].status;
-                    SupplyTaskRepository.Add(supplyTask);
-                    SupplyTaskRepository.SaveChanges();
+                    SupplyTask supplyTask = new SupplyTask();
+                    string id = "";
+                    for (int i = 0; i < vacancyQuantity - notOrderQuantity; i++)
+                    {
+                        supplyTask.SupplyId = SortTask[i].Id;
+                        supplyTask.PackNo = SortTask[i].PackNo;
+                        supplyTask.SortingLineCode = SortTask[i].SortingLineCode;
+                        supplyTask.GroupNo = SortTask[i].GroupNo;
+                        supplyTask.ChannelCode = SortTask[i].ChannelCode;
+                        supplyTask.ChannelName = SortTask[i].ChannelName;
+                        supplyTask.ProductCode = SortTask[i].ProductCode;
+                        supplyTask.ProductName = SortTask[i].ProductName;
+                        supplyTask.ProductBarcode = SortTask[i].ProductBarcode;
+                        supplyTask.OriginPositionAddress = SortTask[i].OriginPositionAddress;
+                        supplyTask.TargetSupplyAddress = SortTask[i].SupplyAddress;
+                        supplyTask.Status = SortTask[i].status;
+                        SupplyTaskRepository.Add(supplyTask);
+                        SupplyTaskRepository.SaveChanges();
+                        id += "[" + SortTask[i].Id + "]";
+                    }
+                    errorInfo = "任务生成成功！任务ID：" + id;
+                    return true;
                 }
-                errorInfo = "任务生成成功！";
-                return true;
+                else
+                {
+                    errorInfo = "";
+                    return true;
+                }
             }
-            else
+            catch (Exception e)
             {
-                errorInfo = "没有新任务生成！";
+                errorInfo = "原因：" + e.Message;
                 return false;
             }
         }

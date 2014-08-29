144ba0a2877f935c73f1f48d71a97d908ae7b3fa
Dong-Yang <772369620@qq.com>
2014-08-29 10:44:58 +0800

    sms新增运行状态查询

---
+++ code/Authority/THOK.SMS.Bll/Service/SmsDeviceStateService.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using THOK.SMS.DbModel;
+using THOK.SMS.Dal.Interfaces;
+using THOK.SMS.Bll.Interfaces;
+using System.Linq;
+using Microsoft.Practices.Unity;
+using THOK.Common.Entity;
+using System.Data;
+using THOK.SMS.Dal.EntityRepository;
+using THOK.Wms.Dal.Interfaces;
+
+namespace THOK.SMS.Bll.Service
+{
+    public class SmsDeviceStateService : ServiceBase<SmsDeviceState>, ISmsDeviceStateService
+    {
+        [Dependency]
+        public ISmsDeviceStateRepository SmsDeviceStateRepository { get; set; }
+
+        protected override Type LogPrefix
+        {
+            get { return this.GetType(); }
+        }
+
+
+        public object GetDetails(int page, int rows, string DeviceCode, string DeviceType, string StateCode, string BeginTime, string EndTime, string UseTime)
+        {
+            IQueryable<SmsDeviceState> smsDeviceStateQuery = SmsDeviceStateRepository.GetQueryable();
+
+            IQueryable<SmsDeviceState> smsDeviceStateDetail1 = smsDeviceStateQuery;
+            if (DeviceCode != "" )
+            {
+                smsDeviceStateDetail1 = smsDeviceStateQuery.Where(s => s.DeviceCode == DeviceCode);
+            }
+            IQueryable<SmsDeviceState> smsDeviceStateDetail2 = smsDeviceStateDetail1;
+            if (!string.IsNullOrEmpty(EndTime))
+            {
+                DateTime endTime = Convert.ToDateTime(EndTime);
+                smsDeviceStateDetail2 = smsDeviceStateDetail1.Where(s => s.EndTime.Equals(endTime));
+            }
+            IQueryable<SmsDeviceState> smsDeviceStateDetail3 = smsDeviceStateDetail2;
+            if (DeviceType != "")
+            {
+                smsDeviceStateDetail3 = smsDeviceStateDetail2.Where(s => s.DeviceType.Contains(DeviceType));
+            }
+            IQueryable<SmsDeviceState> smsDeviceStateDetail4 = smsDeviceStateDetail3;
+            if (StateCode != "")
+            {
+                smsDeviceStateDetail4 = smsDeviceStateDetail3.Where(s => s.StateCode.Contains(StateCode));
+            }
+            IQueryable<SmsDeviceState> smsDeviceStateDetail5 = smsDeviceStateDetail4;
+            if (BeginTime != string.Empty && BeginTime != null)
+            {
+                DateTime beginTime = Convert.ToDateTime(BeginTime);
+                smsDeviceStateDetail5 = smsDeviceStateDetail4.Where(s => s.BeginTime.Equals(beginTime));
+            }
+            IQueryable<SmsDeviceState> smsDeviceStateDetail6 = smsDeviceStateDetail5;
+            if (UseTime != null && UseTime != "")
+            {
+                int useTime = Convert.ToInt32(UseTime);
+                smsDeviceStateDetail6 = smsDeviceStateDetail5.Where(s => s.UseTime.Equals(useTime));
+            }
+            var v1 = smsDeviceStateDetail6.OrderBy(a => a.Id).Select(t => t);
+            int total = v1.Count();
+            v1 = v1.Skip((page - 1) * rows).Take(rows);
+
+            var smsDeviceStatesArray = v1.ToArray().Select(s => new
+                {
+                    s.Id,
+                    s.DeviceCode,
+                    s.DeviceName,
+                    s.DeviceType,
+                    StateCode = s.StateCode == "1" ? "正常运行" : s.StateCode =="2" ? "正常停机" : s.StateCode == "3" ? "故障运行" : s.StateCode == "4" ? "故障停机":"异常",
+                    BeginTime = s.BeginTime.ToString("yyyy-MM-dd"),
+                    EndTime = s.EndTime.ToString("yyyy-MM-dd"),
+                    s.UseTime
+                });
+            return new { total, rows = smsDeviceStatesArray };
+        }
+
+        public DataTable GetSmsDeviceState(int page, int rows, string DeviceCode, string DeviceType, string StateCode, string BeginTime, string EndTime, string UseTime)
+        {
+            IQueryable<SmsDeviceState> smsDeviceStateQuery = SmsDeviceStateRepository.GetQueryable();
+            IQueryable<SmsDeviceState> smsDeviceStateDetail1 = smsDeviceStateQuery;
+            if (DeviceCode != "")
+            {
+                smsDeviceStateDetail1 = smsDeviceStateQuery.Where(s => s.DeviceCode == DeviceCode);
+            }
+            IQueryable<SmsDeviceState> smsDeviceStateDetail2 = smsDeviceStateDetail1;
+            if (!string.IsNullOrEmpty(EndTime))
+            {
+                DateTime endTime = Convert.ToDateTime(EndTime);
+                smsDeviceStateDetail2 = smsDeviceStateDetail1.Where(s => s.EndTime.Equals(endTime));
+            }
+            IQueryable<SmsDeviceState> smsDeviceStateDetail3 = smsDeviceStateDetail2;
+            if (DeviceType != "")
+            {
+                smsDeviceStateDetail3 = smsDeviceStateDetail2.Where(s => s.DeviceType.Contains(DeviceType));
+            }
+            IQueryable<SmsDeviceState> smsDeviceStateDetail4 = smsDeviceStateDetail3;
+            if (StateCode != "")
+            {
+                smsDeviceStateDetail4 = smsDeviceStateDetail3.Where(s => s.StateCode.Contains(StateCode));
+            }
+            IQueryable<SmsDeviceState> smsDeviceStateDetail5 = smsDeviceStateDetail4;
+            if (BeginTime != string.Empty && BeginTime != null)
+            {
+                DateTime beginTime = Convert.ToDateTime(BeginTime);
+                smsDeviceStateDetail5 = smsDeviceStateDetail4.Where(s => s.BeginTime.Equals(beginTime));
+            }
+            IQueryable<SmsDeviceState> smsDeviceStateDetail6 = smsDeviceStateDetail5;
+            if (UseTime != null && UseTime != "")
+            {
+                int useTime = Convert.ToInt32(UseTime);
+                smsDeviceStateDetail6 = smsDeviceStateDetail5.Where(s => s.UseTime.Equals(useTime));
+            }
+            var v1 = smsDeviceStateDetail6.OrderBy(a => a.Id).Select(t => t);
+            var smsDeviceStatesArray = v1.OrderBy(s => s.Id).AsEnumerable()
+                .Select(s => new
+                {
+                    s.Id,
+                    s.DeviceCode,
+                    s.DeviceName,
+                    s.DeviceType,
+                    StateCode = s.StateCode == "1" ? "正常运行" : s.StateCode == "2" ? "正常停机" : s.StateCode == "3" ? "故障运行" : s.StateCode == "4" ? "故障停机" : "异常",
+                    BeginTime = s.BeginTime.ToString("yyyy-MM-dd"),
+                    EndTime = s.EndTime.ToString("yyyy-MM-dd"),
+                    s.UseTime
+                }).ToArray();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Id", typeof(int));
+            dt.Columns.Add("设备代码", typeof(string));
+            dt.Columns.Add("设备名称", typeof(string));
+            dt.Columns.Add("设备类型", typeof(string));
+            dt.Columns.Add("状态代码", typeof(string));
+            dt.Columns.Add("开始时间", typeof(string));
+            dt.Columns.Add("结束时间", typeof(string));
+            dt.Columns.Add("合计用时", typeof(int));
+
+            foreach (var item in smsDeviceStatesArray)
+            {
+                dt.Rows.Add(
+                    item.Id,
+                    item.DeviceCode,
+                    item.DeviceName,
+                    item.DeviceType,
+                    item.StateCode,
+                    item.BeginTime,
+                    item.EndTime,
+                    item.UseTime);
+            }
+            return dt;
+        }
+    }
+}
---
+++ code/Authority/Wms/Controllers/SMS/DeviceStatus/SmsDeviceStateController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Practices.Unity;
+using THOK.Common.NPOI.Models;
+using THOK.Common.NPOI.Service;
+using THOK.Common.WebUtil;
+using THOK.Security;
+using THOK.SMS.Bll.Interfaces;
+using THOK.SMS.DbModel;
+
+namespace Wms.Controllers.SMS.DeviceState
+{
+    [TokenAclAuthorize]
+    public class SmsDeviceStateController : Controller
+    {
+        [Dependency]
+        public ISmsDeviceStateService SmsDeviceStateServer { get; set; }
+        //
+        // GET: /SmsDeviceState/
+
+        public ActionResult Index(string moduleID)
+        {
+            ViewBag.hasSearch = true;
+            ViewBag.hasPrint = true;
+            ViewBag.hasHelp = true;
+            ViewBag.ModuleID = moduleID;
+            return View();
+        }
+
+        public ActionResult Details(int page, int rows, FormCollection collection)
+        {
+            SmsDeviceState smsDeviceState = new SmsDeviceState();
+
+            string DeviceCode = collection["DeviceCode"] ?? "";
+            string DeviceName = collection["DeviceName"] ?? "";
+            string DeviceType = collection["DeviceType"] ?? "";
+            string StateCode = collection["StateCode"] ?? "";
+            string BeginTime = collection["BeginTime"] ?? "";
+            string EndTime = collection["EndTime"] ?? "";
+            string UseTime = collection["UseTime"] ?? "";
+
+            var smsDeviceStateDetail = SmsDeviceStateServer.GetDetails(page, rows, DeviceCode, DeviceType, StateCode, BeginTime, EndTime, UseTime);
+            return Json(smsDeviceStateDetail, "text", JsonRequestBehavior.AllowGet);
+
+        }
+
+
+        //public ActionResult Details(int page, int rows, FormCollection collection)
+        //{
+        //    SmsDeviceState smsDeviceState = new SmsDeviceState();
+        //    string UseTime = collection["UseTime"] ?? "";
+        //    if (UseTime != "" && UseTime != null)
+        //    {
+        //        smsDeviceState.UseTime = Convert.ToInt32(UseTime);
+        //    }
+        //    smsDeviceState.DeviceCode = collection["DeviceCode"] ?? "";
+        //    smsDeviceState.DeviceName = collection["DeviceName"] ?? "";
+        //    smsDeviceState.DeviceType = collection["DeviceType"] ?? "";
+        //    smsDeviceState.StateCode = collection["StateCode"] ?? "";
+        //    smsDeviceState.BeginTime = collection["BeginTime"] ?? "";
+        //    smsDeviceState.EndTime = collection["EndTime"] ?? "";
+
+        //    var smsDeviceStateDetail = SmsDeviceStateServer.GetDetails(page, rows, smsDeviceState);
+        //    return Json(smsDeviceStateDetail, "text", JsonRequestBehavior.AllowGet);
+
+        //}
+
+        //打印
+        //#region /SmsDeviceState/CreateExcelToClient/
+        //public FileStreamResult CreateExcelToClient()
+        //{
+        //    int page = 0, rows = 0;
+        //    //int SupplyId = Convert.ToInt32(Request.QueryString["SupplyId"]);
+        //    //int PackNo = Convert.ToInt32(Request.QueryString["PackNo"]);
+        //    string channelCode = Request.QueryString["ChannelCode"];
+        //    string productCode = Request.QueryString["ProductCode"];
+        //    string productName = Request.QueryString["ProductName"];
+        //    string sortingLineCode = Request.QueryString["SortingLineCode"];
+        //    //int groupNo = Convert.ToInt32(Request.QueryString["GroupNo"]);
+        //    string status = Request.QueryString["Status"];
+        //    SmsDeviceState smsDeviceState = new SmsDeviceState();
+        //    //smsDeviceState.SupplyId = SupplyId;
+        //    //smsDeviceState.PackNo = PackNo;
+        //    smsDeviceState.ChannelCode = channelCode;
+        //    smsDeviceState.ProductCode = productCode;
+        //    smsDeviceState.ProductName = productName;
+        //    smsDeviceState.SortingLineCode = sortingLineCode;
+        //    //smsDeviceState.GroupNo = groupNo;
+        //    smsDeviceState.Status = status;
+
+        //    ExportParam ep = new ExportParam();
+        //    ep.DT1 = SmsDeviceStateServer.GetSmsDeviceState(page, rows, smsDeviceState);
+        //    ep.HeadTitle1 = "分拣补货查询";
+        //    return PrintService.Print(ep);
+        //}
+        //#endregion
+
+        public FileStreamResult CreateExcelToClient()
+        {
+            int page = 0, rows = 0;
+            ExportParam ep = new ExportParam();
+
+            string DeviceCode = Request.QueryString["DeviceCode"];
+            string DeviceName = Request.QueryString["DeviceName"];
+            string DeviceType = Request.QueryString["DeviceType"];
+            string StateCode = Request.QueryString["StateCode"];
+            string BeginTime = Request.QueryString["BeginTime"];
+            string EndTime = Request.QueryString["EndTime"];
+            string UseTime = Request.QueryString["UseTime"];
+            ep.DT1 = SmsDeviceStateServer.GetSmsDeviceState(page, rows, DeviceCode, DeviceType, StateCode, BeginTime, EndTime, UseTime);
+            ep.HeadTitle1 = "运行状态查询";
+            return PrintService.Print(ep);
+        }
+
+    }
+}
+

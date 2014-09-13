525a09572bd2ce50453b1b13147b9270900bee48
zengys <ynnuyonw@126.com>
2014-09-13 16:24:48 +0800

    配置 THOK.SMS.REST 基本功能。

---
+++ code/Authority/Wms/Controllers/REST/SMS/SupplyController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web.Mvc;
+using THOK.SMS.Bll.Models;
+using Microsoft.Practices.Unity;
+using THOK.SMS.REST.Interfaces;
+
+namespace Wms.Controllers.REST.SMS
+{
+    public class SupplyController : Controller
+    {
+        [Dependency]
+        public ISupplyService SupplyService { get; set; }
+
+        public ActionResult CreateSupplyTask(int position, int quantity, DateTime orderdate, int batchNo)
+        {
+            string errorInfo = string.Empty;
+            bool bResult = SupplyService.CreateSupplyTask(position, quantity, orderdate, batchNo, out errorInfo);
+            return Json(new Result { IsSuccess = bResult, Message = errorInfo }, "application/json", JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult AssignTaskOriginPositionAddress()
+        {
+            string errorInfo = string.Empty;
+            bool bResult = SupplyService.AssignTaskOriginPositionAddress(out errorInfo);
+            return Json(new Result { IsSuccess = bResult, Message = errorInfo }, "application/json", JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult CheckSupplyPositionStorage()
+        {
+            string errorInfo = string.Empty;
+            bool bResult = SupplyService.CheckSupplyPositionStorage(out errorInfo);
+            return Json(new Result { IsSuccess = bResult, Message = errorInfo }, "application/json", JsonRequestBehavior.AllowGet);
+        }
+    }
+}

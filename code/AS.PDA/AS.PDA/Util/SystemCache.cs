b1f79cbba4edd15682afc120af5574483655d025
2Tiramisu <545510993@qq.com>
2014-10-09 17:27:03 +0800

    PDA

---
+++ code/AS.PDA/AS.PDA/Util/SystemCache.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+using AS.PDA.View;
+using System.Data;
+
+namespace AS.PDA.Util
+{
+    public static class SystemCache
+    {
+        private static MainForm mainFrom = null;
+        public static MainForm MainFrom
+        {
+            get { return SystemCache.mainFrom; }
+            set { SystemCache.mainFrom = value; }
+        }
+
+        private static string connetionType = "";
+        public static string ConnetionType
+        {
+            get
+            {
+                if (connetionType == "")
+                {
+                    SystemCache.connetionType = new ConfigUtil().GetConfig("ConnetionType")["Type"];
+                }
+                return SystemCache.connetionType;
+            }
+        }
+
+        private static string httpConnectionStr = "";
+        public static string HttpConnectionStr
+        {
+            get
+            {
+                SystemCache.httpConnectionStr = new ConfigUtil().GetConfig("HttpConnectionStr")["HttpConnStr"];
+                return SystemCache.httpConnectionStr;
+            }
+        }
+    }
+}
